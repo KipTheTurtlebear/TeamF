d16a97b9b50565bf24cfca3d3754d0d9d72ec976
Kyle McBurnett <mastereuclid@gmail.com>
2019-02-06 15:41:22 -0800

    header update. Probably going to need more.

--- Input.h
+++ Input.h
@@ -1,12 +1,19 @@
 #include<iostream>
 #include "ClassDefinitions.h"
 using namespace std;
-//a comment
+
 class Input
 {
 private:
     // A reference to the base program; Through this we can access any class instances we need, like Output
     Program* referenceToProgram;
+    //we can use the direction keys and immediately update or collect text from the console emulator
+    int mode;
+    char ** text;
+    Input(); // no copying me
 public:
     Input(Program* program);
+    ~Input();
+    void getInput();
+
 }
